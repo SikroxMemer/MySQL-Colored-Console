2b871f4168a1bd40addff2ab9d2c333be2a95cb0
SikroxMemer <simoarmymen9@gmail.com>
2024-02-17 14:44:10 +0100

    Removed Unprotected Password Removed config.py Removed Unused scripts Optimized Code

--- main.py
+++ main.py
@@ -1,41 +1,65 @@
-# Modules :
 from tabulate import tabulate
-from mysql.connector import Error as Err
 from rich.console import Console
-from config import connection
-import sys
+from mysql.connector.errors import Error
+from mysql.connector.connection import MySQLConnection
 import os
-sys.dont_write_bytecode = True
-# # # # # # # # # # # # # # # # #
+
+
+class Connection(MySQLConnection):
+    """
+    Standard MySQL Connection 
+    """
+    # This Is Sensible Data Don't Share it if you are working in a production server
+    __SQL_INFO = {"host": "localhost", "user": "root",
+                  "password": "YOUR_SUPER_SECRET_PASSWORD"}
+    _CONSOLE = Console()
+
+    def __init__(self, **kwargs) -> None:
+        try:
+            super().__init__(**self.__SQL_INFO)
+        except Error as error:
+            self._CONSOLE.print("[red]{}[/]".format(error))
 
 
 class MySQLColoredConsole():
+
     _REVERSE_STATE = True
     _CONSOLE = Console()
-    _CONNECTION = connection
+    _CONNECTION = Connection()
     _CONNECTION.autocommit = True
 
     def __init__(self):
         try:
+
             while self._REVERSE_STATE:
+
                 self.sql = self._CONSOLE.input(
                     f"<{self._CONNECTION.user}@[yellow]{self._CONNECTION._host}[/]> : ")
+
                 if self.sql == "CLEAR":
                     os.system('cls' if os.name == 'nt' else 'clear')
+
                 elif self.sql == "EXIT":
                     self._CONSOLE.print("[yellow]Bye ^^[/]")
                     exit()
+
                 else:
                     try:
-                        self.cursor = self._CONNECTION.cursor(dictionary=True)
+
+                        self.cursor = self._CLIENT.cursor(dictionary=True)
                         self.cursor.execute(self.sql)
                         self._CONSOLE.log(f"<success> [green]{self.sql}[/]")
                         result: list[dict] = self.cursor.fetchall()
+
                         if len(result) > 0:
+
                             self._CONSOLE.print(
                                 f"\n{tabulate(result , headers='keys',tablefmt='simple')}\n")
-                    except Err as error:
-                        self._CONSOLE.log(f"<error> [red]{error.msg}[/]")
+
+                    except Error as error:
+
+                        self._CONSOLE.log(f"<error>[red]{error.msg}[/]")
+
         except KeyboardInterrupt:
             self._CONSOLE.print("\n[red]Ouch :([/]")
 
