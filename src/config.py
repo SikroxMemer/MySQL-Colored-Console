8ea4765a479de6c389b608f68f71745aec6f4653
SikroxMemer <simoarmymen9@gmail.com>
2024-02-17 13:57:11 +0100

    Removed Unused Scripts Optimized Code Removed Functional Structor No Executable

--- src/config.py
+++ src/config.py
@@ -1,9 +1,12 @@
-SQL_INFO = {"host" : "localhost" , "user" : "root" ,"password" : ""}
+SQL_INFO = {"host" : "localhost" , "user" : "root" ,"password" : "789456123Simo11"}
+
+
 
 import mysql.connector
 
 try:
     connection = mysql.connector.connect(**SQL_INFO)
+
 except mysql.connector.Error as error:
     print(error)
     exit()
--- src/main.py
+++ src/main.py
@@ -1,49 +1,43 @@
 # Modules :
-
-import sys , os
-sys.dont_write_bytecode = True
-
-from config import connection
-from rich.console import Console
-
-from rich.table import Table
-import rich.box as bx
-
-from mysql.connector import Error as Err
 from tabulate import tabulate
-
+from mysql.connector import Error as Err
+from rich.console import Console
+from config import connection
+import sys
+import os
+sys.dont_write_bytecode = True
 # # # # # # # # # # # # # # # # #
 
-current = Console()
 
-while True:
-    query_state = current.input(f"<{connection._host}@[yellow]{connection.user}[/]> : ")
+class MySQLColoredConsole():
+    _REVERSE_STATE = True
+    _CONSOLE = Console()
+    _CONNECTION = connection
+    _CONNECTION.autocommit = True
 
-    if query_state == "--clear":
-        os.system('cls' if os.name == 'nt' else 'clear')
-    elif query_state == "EXIT":
-        current.print("[yellow]Bye ^^ ![/]")
-        exit()
-    else:
+    def __init__(self):
         try:
+            while self._REVERSE_STATE:
+                self.sql = self._CONSOLE.input(
+                    f"<{self._CONNECTION.user}@[yellow]{self._CONNECTION._host}[/]> : ")
+                if self.sql == "CLEAR":
+                    os.system('cls' if os.name == 'nt' else 'clear')
+                elif self.sql == "EXIT":
+                    self._CONSOLE.print("[yellow]Bye ^^[/]")
+                    exit()
+                else:
+                    try:
+                        self.cursor = self._CONNECTION.cursor(dictionary=True)
+                        self.cursor.execute(self.sql)
+                        self._CONSOLE.log(f"<success> [green]{self.sql}[/]")
+                        result: list[dict] = self.cursor.fetchall()
+                        if len(result) > 0:
+                            self._CONSOLE.print(
+                                f"\n{tabulate(result , headers='keys',tablefmt='simple')}\n")
+                    except Err as error:
+                        self._CONSOLE.log(f"<error> [red]{error.msg}[/]")
+        except KeyboardInterrupt:
+            self._CONSOLE.print("\n[red]Ouch :([/]")
 
-            cursor = connection.cursor(dictionary=True)
-            cursor.execute(query_state)
-            current.log(f"<success> [green]{query_state}[/]")
 
-            try:
-                result : list[dict] = cursor.fetchall()
-                table = Table(box=bx.SQUARE)
-
-                current.print(f"\n{tabulate(result , headers='keys',tablefmt='rounded_grid')}\n")
-                
-                # current.print(table)
-                connection.commit() 
-                
-            except Err as error:
-                pass
-            
-        
-        except Err as error:
-
-            current.log(f"<error> : [red]{error.msg}[/]")
+sql_client = MySQLColoredConsole()
